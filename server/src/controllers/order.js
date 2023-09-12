4df2e66a9c3b1a32645d41031f6803c3b58460d6
aljer80 <jernberg.alexandra@gmail.com>
2023-09-12 10:23:44 +0200

    functions related to orders, not complete yet

--- server/src/controllers/order.js
+++ server/src/controllers/order.js
@@ -1,27 +1,63 @@
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const fs = require("fs");
 
+//funktion för att skapa en order/beställning
+async function createOrder(req, res, next) {
+  try {
+    const newOrder = {
+      // Skapa orderstruktur här 
+    };
 
-//FUNKTION FÖR ATT PLACERA ORDER
-//if som kollar om man är inloggad. Om inloggad körs koden:
-//async function createOrder
-     //get user
-     //get product (för alla produkter i kundvagnen)
-    //skicka order till Stripe
+    // Lägger till order i JSON-filen för att spara orderhistoriken
+    const orders = readOrdersFromFile();
+    orders.push(newOrder);
+    writeOrdersToFile(orders);
 
-
-
-//FUNKTION FÖR ATT KUNNA SE SINA LAGDA ORDRAR
-async function getOrders (req, res, next) {
-    try {
-        //const allOrders = hämta alla ordrar och populera med customer?
-        if (allOrders.length == 0) {
-            res.status(404).json("No orders found");
-        } else {
-            res.status(200).json(allOrders);
-        }      
-    } catch (error) {
-            res.status(500).json();
-        }
+    res.status(201).json(newOrder);
+  } catch (error) {
+    console.log(error);
+    res.status(401).json(error);
+  }
 }
 
+function readOrdersFromFile() {
+  try {
+    const ordersData = fs.readFileSync(ordersFilePath, "utf8");
+    return JSON.parse(ordersData);
+  } catch (error) {
+    return [];
+  }
+}
 
+function writeOrdersToFile(orders) {
+  const jsonData = JSON.stringify(orders, null, 2);
+  fs.writeFileSync(ordersFilePath, jsonData);
+}
+
+//funktion för att hämta alla ordrar
+function getOrders(req, res, next) {
+    try {
+      const orders = readOrdersFromFile();
+      res.status(200).json(orders);
+    } catch (error) {
+      res.status(500).json();
+    }
+  }
+ 
+//funktion för att hämta en specifik order
+  function getOrderById(req, res, next) {
+    try {
+      const orders = readOrdersFromFile();
+      const order = orders.find((order) => order.id === req.params.id);
   
+      if (!order) {
+        return res.status(404).json(req.params.id + " not found");
+      }
+
+      res.status(200).json(order);
+    } catch (error) {
+      res.status(500).json();
+    }
+  }
+  
+  module.exports = { createOrder, getOrders, getOrderById }; 
