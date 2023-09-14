a0020afba3df9e8702b948e529d7c394591af872
aljer80 <jernberg.alexandra@gmail.com>
2023-09-14 10:54:54 +0200

    function for rendering Products

--- client/src/Components/Products.jsx
+++ client/src/Components/Products.jsx
@@ -2,40 +2,45 @@ import React, { useEffect, useState } from "react";
 
 //PÅBÖRJAD FUNKTION, EJ KLAR! 
 function renderProducts() {
-
     const [products, setProducts] = useState([]);
-
+  
     useEffect(() => {
-        async function getProducts() {
+      async function getProducts() {
         try {
-            const response = await fetch("http://localhost:3000/api/products/product-list"); // hittade på product-list
-            if (response.ok) {
-            const data = await response.json();
-            setProducts(data);
-            }
+          const response = await fetch("http://localhost:3000/api/products");
+          if (!response.ok) {
+            throw new Error("Något gick fel vid hämtning av data.");
+          }
+          const data = await response.json();
+          setProducts(data);
         } catch (error) {
-            console.error("Error fetching products:", error);
+          console.error(error);
         }
-        }
-
-        getProducts();
+      }
+      getProducts();
     }, []);
-
+  
     return (
-        <div>
-            <h2>Produkter</h2>
-            <ul>
-                {products.map((product) => (
-                <li key={product.id}>
-                    <h3>{product.name}</h3>
-                    <p>{product.description}</p>
-                    <p>Pris: {product.price}</p>
-                    {/* Lägg till andra produktinformationselement här */}
-                </li>
-                ))}
-            </ul>
+      <div>
+        <h2>Produkter</h2>
+        <div className="product-container">
+          {products.map((product) => (
+            <div key={product.id} className="product">
+              <div className="product-image">
+                <img src={product.image} alt={product.name} />
+              </div>
+              <div className="product-details">
+                <h3>{product.name}</h3>
+                <p>{product.description}</p>
+                <p>Pris: {product.price}</p>
+              </div>
+            </div>
+          ))}
         </div>
+      </div>
     );
-}
+  }
+
+  
 
 export default renderProducts
