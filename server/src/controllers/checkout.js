14b5b643602c6c6b756d018ba6ab4839bb7b9eec
aljer80 <jernberg.alexandra@gmail.com>
2023-09-18 15:59:43 +0200

    added functionality for promotion codes

--- server/src/controllers/checkout.js
+++ server/src/controllers/checkout.js
@@ -1,5 +1,5 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-const CLIENT_URL = "http://localhost:5173/";
+const CLIENT_URL = "http://localhost:5173";
 
 //Checkout Session controls what your customer sees on the payment page such as line items, the order amount and currency, and acceptable payment methods. 
   const checkoutSession = async (req, res) => {
@@ -12,7 +12,8 @@ const CLIENT_URL = "http://localhost:5173/";
           };
         }),
         mode: "payment",
-        success_url: `${CLIENT_URL}/confirmation`,
+        allow_promotion_codes: true, 
+        success_url: `${CLIENT_URL}/confirmation`, //man kan - om man vill - få sessions id:t från Stripe här, lagrad i url:en, som en query parameter: success_url: `${CLIENT_URL}/confirmation?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: CLIENT_URL,
       });
   
@@ -23,40 +24,37 @@ const CLIENT_URL = "http://localhost:5173/";
     }
 
   };
+
   
 //Verify Session 
+//Funktion som tar emot sessions-id:t, kollar om den är betalad, hämtar ut line-items (hämta ut det man behöver för att pussla ihop en order)
 const verifySession = async (req, res) => {
   try {
-      console.log(req.body.sessionId);
-      res.status(200).json({ verified: true });
       //hämta sessionen från Stripe och verifiera den
-      const session = await stripe.checkout.sessions.retrieve(req.body.sessionId); //plocka ut vad man vill ha. T.ex. på price. Plocka ut och göra uträkning
-        //plocka ut vem som köpt, och vad man köpt
+      const session = await stripe.checkout.sessions.retrieve(req.body.sessionId); 
+      console.log(req.body.sessionId);
+      
+      //plocka ut vem som köpt, och vad man köpt (om sessionen är paid)
       if(session.payment_status !== "paid"){
         return res.status(400).json({ verified:false });
       }
-      //hämtar line_items till ordern nedan
-      const line_items = await stripe.checkout.sessions.listLineItems(req.body.sessionId)
-
-      // skapa en order och spara undan den (ta med det man själv vill ha med)
+      //hämtar line_items till ordern nedan (det man behöver/vill ha för att pussla ihop en order)
+      const line_items = await stripe.checkout.sessions.listLineItems(req.body.sessionId);
+      // skapar en order och sparar undan den
       const order = {
         created: session.created, 
         customer: session.customer_details.name,
         products: line_items.data.map(item => {
           return {
-            //vill man gå djupare får man använda objektet product
             product: item.description, //namnet på produkten
             quantity: item.quantity, 
             price: item.price.unit_amount / 100 * item.quantity, 
-            
-          }
-        })
+            //vill man gå djupare får man använda objektet product
+          };
+        }),
       };
-      //SAVE ORDER
-      //SKRIV TILL JSON-filen
-      
+      //SAVE ORDER, SKRIV TILL JSON-filen
       console.log("Loggar ut order: ", order);
-
       res.status(200).json({ verified:true });
   } catch (error) {
     console.log(error.message);
